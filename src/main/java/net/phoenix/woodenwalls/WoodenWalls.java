f2d395cb62e751c46a3f7354d4e85362acb62d95
PhoenixOrigin <phoenixorigin171@gmail.com>
2023-09-30 14:37:11 +0100

    Initial commit

---
+++ src/main/java/net/phoenix/woodenwalls/WoodenWalls.java
@@ -0,0 +1,31 @@
+package net.phoenix.woodenwalls;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.*;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.item.BlockItem;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class WoodenWalls implements ModInitializer {
+    public static final String MOD_ID = "wooden_walls";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    public static Block OAK_WALL = new WallBlock(FabricBlockSettings.copyOf(Blocks.OAK_WOOD));
+    public static Block BIRCH_WALL = new WallBlock(FabricBlockSettings.copyOf(Blocks.BIRCH_WOOD));
+
+
+    @Override
+    public void onInitialize() {
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "oak_wall"), OAK_WALL);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "oak_wall"), new BlockItem(OAK_WALL, new FabricItemSettings()));
+
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, "birch_wall"), BIRCH_WALL);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "birch_wall"), new BlockItem(BIRCH_WALL, new FabricItemSettings()));
+    }
+
+}
---
+++ src/main/java/net/phoenix/woodenwalls/client/WoodenWallsClient.java
@@ -0,0 +1,14 @@
+package net.phoenix.woodenwalls.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WoodenWallsClient implements ClientModInitializer {
+
+    /**
+     * Runs the mod initializer on the net.phoenix.woodenwalls.client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+    }
+}
+
